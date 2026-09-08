d4aff933961608d873a764ebda417ce0b7f31a22
iamnjinx <136955351+iamnjinx@users.noreply.github.com>
2026-09-08 14:34:22 +0900

    20260906-2

--- Assets/Decalcomanie/Game Manager/SkinButton.cs
+++ Assets/Decalcomanie/Game Manager/SkinButton.cs
@@ -11,7 +11,8 @@ public class SkinButton : ButtonUI
     public Image DinoImage;
     public TextMeshProUGUI starCostText;
 
-    public BaseUI selectedFlagUI;
+    public Image[] panelUIs = new Image[3];
+    public Color[] unlockedColors = new Color[3];
 
     public DinoSkinSO dinoSkinSO;
 
@@ -23,16 +24,16 @@ public class SkinButton : ButtonUI
 
     public void SetSelectedFlagActive(bool isSelected)
     {
-        if (selectedFlagUI != null)
-        {
-            selectedFlagUI.SetUI(isSelected);
-        }
+        panelUIs[0].color = isSelected ? unlockedColors[0] : Color.white;
+        panelUIs[2].color = isSelected ? unlockedColors[2] : Color.white;
     }
 
     public void SetUnlocked(bool isUnlocked)
     {
         SkinImage.color = isUnlocked ? Color.white : Color.black;
         DinoImage.color = isUnlocked ? Color.white : Color.black;
+
+        panelUIs[1].color = isUnlocked ? unlockedColors[1] : Color.white;
 
         interactable = isUnlocked;
     }
